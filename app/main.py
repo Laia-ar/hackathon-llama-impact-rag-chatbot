3c2c8dd970ee7e79baaf926cead5eea76ede6bd0
David Coronel <david.coronel@laia.ar>
2024-11-23 15:12:38 -0300

    Adding whisper transcription

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from get_random_answer import query_llm
 from audiorecorder import audiorecorder
 from pydub import AudioSegment
 import io
+import whisper
 
 st.set_page_config(page_title="Chatbot", layout="centered")
 st.title("Chatbot with RAG")
@@ -10,9 +11,18 @@ st.title("Chatbot with RAG")
 # Input para el chatbot
 user_input = st.text_input("Hacé una pregunta:", placeholder="Tipeá algo...")
 
+# Tipo de modelo
+whisper_model_type = st.radio("Please choose your model type", ('Tiny', 'Base', 'Small', 'Medium', 'Large'))
+
 # Historial del chat
 if "chat_history" not in st.session_state:
     st.session_state["chat_history"] = []
+
+
+def process_audio(filename, model_type):
+    model = whisper.load_model(model_type)
+    result = model.transcribe(filename)
+    return result["text"]
 
 # Grabacion de audio para la transcripcion
 audio = audiorecorder("Grabar audio", "Detener")
@@ -20,15 +30,13 @@ if len(audio) > 0:
     try:
         if isinstance(audio, AudioSegment):
             # AudioSegment a bytes
-            buffer = io.BytesIO()
-            audio.export(buffer, format="wav")
-            audio_bytes = buffer.getvalue()
+            st.audio(audio.export().read())  
+            audio.export("audio.wav", format="wav")
             
-            st.audio(audio_bytes, format="audio/wav")
             st.success("¡Audio grabado y reproducido!")
 
-            # Placeholder para la transcripcrión
-            transcribed_text = "[Transcripción de audio pendiente]"
+            # Placeholder para la transcripción
+            transcribed_text = process_audio("audio.wav", whisper_model_type.lower())
             st.session_state["chat_history"].append({"role": "user", "content": transcribed_text})
         else:
             st.error("Formato de audio no compatible.")
@@ -47,3 +55,4 @@ for chat in st.session_state["chat_history"]:
         st.markdown(f"**Vos:** {chat['content']}")
     else:
         st.markdown(f"**Medic-bot:** {chat['content']}")
+
